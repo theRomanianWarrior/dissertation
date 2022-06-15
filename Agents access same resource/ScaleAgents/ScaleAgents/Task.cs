1e18944cac156e8ddb3d99dff022aca2b20a4611
Emilian Mihailov <Emilian.Mihailov@endava.com>
2022-06-15 14:55:06 +0300

    Try to acces same resource by agents

---
+++ Agents access same resource/ScaleAgents/ScaleAgents/Task.cs
@@ -0,0 +1,20 @@
+﻿namespace ScaleAgents
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public string? AgentId { get; set; } // Agent assigned to the task
+        public TaskType TypeId { get; set; } //type is the task’s type (flight, hotel or attractions)
+        public string UserPreferenceId { get; set; } // what is exactly the user want?
+        public string Status { get; set; } // available, not available
+
+        public Task(Guid id,string? agent, TaskType type, string? userPreference, string stat)
+        {
+            Id = id;
+            AgentId = agent;
+            TypeId = type;
+            UserPreferenceId = userPreference;
+            Status = stat;
+        }
+    }
+}
